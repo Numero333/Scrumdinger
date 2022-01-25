176619cf3b6355e076b4beda110cb77e562b3e95
333Corp <fx.meite@gmail.com>
2022-01-25 19:36:49 +0100

    Update UI

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct ScrumdingerApp: App {
     @StateObject private var store = ScrumStore()
+    @State private var errorWrapper: ErrorWrapper?
     //@State private var scrums = DailyScrum.sampleData
     
     var body: some Scene {
@@ -20,6 +21,7 @@ struct ScrumdingerApp: App {
                         do {
                             try await ScrumStore.save(scrums: store.scrums)
                         } catch {
+                            errorWrapper = ErrorWrapper(error: error, guidance: "Try again later.")
                             fatalError("Error saving scrums.")
 //                    ScrumStore.save(scrums: store.scrums) { result in
 //                        if case .failure(let error) = result {
@@ -32,8 +34,13 @@ struct ScrumdingerApp: App {
                 do {
                     store.scrums = try await ScrumStore.load()
                 } catch {
-                    fatalError("Error loading scrums.")
+                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
                 }
+            }
+            .sheet(item: $errorWrapper, onDismiss: {
+                store.scrums = DailyScrum.sampleData
+            }) { wrapper in
+                ErrorView(errorWrapper: wrapper)
             }
         }
     }
